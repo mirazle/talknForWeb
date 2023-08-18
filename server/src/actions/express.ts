1a0501fb73b1c0eb4e838f94fd5eb10f9c1c946d
Miyazaki Hiroyuki <JPZ7123@MN01580833.local>
2023-08-18 16:41:21 +0900

    add node server cluster

--- server/src/actions/express.ts
+++ server/src/actions/express.ts
@@ -1,9 +1,26 @@
+import cluster from 'cluster';
+import os from 'os'
+
 import Express from 'server/listens/express';
+
+const numCPUs = os.cpus().length
 
 export default {
   setUp: async () => {
-    const express = new Express();
-    express.createHttpServer();
-    express.createHttpsServer();
+    if (cluster.isPrimary) {
+      // マスタープロセスの場合、ワーカーをフォークする
+      for (let i = 0; i < numCPUs; i++) {
+        cluster.fork();
+      }
+
+      cluster.on('exit', (worker, code, signal) => {
+        console.log(`Worker ${worker.process.pid} died`);
+      });
+    } else {
+
+      const express = new Express();
+      express.createHttpServer();
+      express.createHttpsServer();
+    }
   },
 };
